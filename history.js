7d62203e420f2fd63e511193d898e164667eef19
MacasaetE <elia.mac365@gmail.com>
2025-11-29 21:58:35 -0600

    History and Images

---
+++ history.js
@@ -0,0 +1,38 @@
+gsap.from(".green-bg", {
+  y: "-100%",
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+});
+
+gsap.from("h1 span", {
+  y: 10,
+  opacity: 0,
+  delay: 0.8,
+  stagger: 0.2,
+});
+
+gsap.from(".history-hero", {
+  y: "200%",
+  opacity: 0,
+  duration: 1,
+  delay: 1,
+});
+
+gsap.from(".card:nth-child(1)", {
+  x: -200,
+  opacity: 0,
+  delay: 2,
+});
+
+gsap.from(".card:nth-child(3)", {
+  x: 200,
+  opacity: 0,
+  delay: 2,
+});
+
+gsap.from(".card:nth-child(2)", {
+  y: 100,
+  opacity: 0,
+  delay: 2,
+});
