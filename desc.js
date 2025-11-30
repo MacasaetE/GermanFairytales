d8ec14e3d429257a07540d83e2a1f4d999c8caab
MacasaetE <elia.mac365@gmail.com>
2025-11-30 17:29:04 -0600

    Update

--- desc.js
+++ desc.js
@@ -19,6 +19,24 @@ gsap.from(".desc-hero", {
   delay: 1,
 });
 
+gsap.from(".card:nth-child(1)", {
+  x: -200,
+  opacity: 0,
+  delay: 2,
+});
+
+gsap.from(".card:nth-child(3)", {
+  x: 200,
+  opacity: 0,
+  delay: 2,
+});
+
+gsap.from(".card:nth-child(2)", {
+  y: 100,
+  opacity: 0,
+  delay: 2,
+});
+
 const host = window.location.hostname;
 document.querySelectorAll('a[href^="http"]').forEach((link) => {
   if (!link.hostname.includes(host)) {
